b4d2c22093db4cc929512e34e068f9b02f298f3b
Dan555 <33890115+DanG555@users.noreply.github.com>
2021-10-19 05:02:53 -0500

    Back to first

--- app.py
+++ app.py
@@ -1,41 +1,32 @@
-# Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-
-# ModbusSlaveServer.py
-# Runs a modbus slave device in memory for testing purposes.
-# This AWS Lambda function allocates registers for the modbus tcp slave and starts
-# the server. If an exception occurs, it will wait 5 seconds and try again.
-# Since the function is long-lived it will run forever when deployed to a 
-# Greengrass core.  The handler will NOT be invoked in our example since 
-# the we are executing an infinite loop.
-
-import time
+from pyModbusTCP.server import ModbusServer, DataBank
+from time import sleep
+from random import uniform
 import logging
 import sys
-#import pymodbus libraries for the server
-from pymodbus.server.sync import StartTcpServer
-from pymodbus.datastore import ModbusSequentialDataBlock
-from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+#Create an instance of ModbusServer
 
-# instantiate logger which will log any exceptions to Cloudwatch or Greengrass 
-# local logs
 logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-while True:
-    try:
-        #create registy for modbus server
-        store = ModbusSlaveContext(
-            di = ModbusSequentialDataBlock(0, [17]*100),
-            co = ModbusSequentialDataBlock(0, [17]*100),
-            hr = ModbusSequentialDataBlock(0, [17]*100),
-            ir = ModbusSequentialDataBlock(0, [17]*100))
-        context = ModbusServerContext(slaves=store, single=True)
-        
-        # change default port of modbus from 502 to 5020 as it requires 
-        # root permissions below 1024
-        StartTcpServer(context, address=("localhost", 5020))
-    except Exception as e:
-        logging.info("Error: {0}".format(str(e)))
-    time.sleep(5)
 
+try:
+    logging.info("Iniciando")
+    server = ModbusServer(host="localhost", port=5020)
+    logging.info("Start server...")
+    #print("Start server...")
+    server.start()
+    logging.info("Server is online")
+    print("Server is online")
+    state=[0]
+    while True:
+        DataBank.set_words(0,[int(uniform(0,100))])
+        if state != DataBank.get_words(1):
+            state = DataBank.get_words(1)
+            print("Value of Registers 1 has changed to =" + str(state))
+            sleep(0.5)
+            
+except Exception as e:
+    logging.info("Error: {0}".format(str(e)))
+    print("Shutdown server ....")
+    server.stop()
+    print("Server is offline")
